c2fb171d132a198c3a4dfd2d0f885eb9e62a9a15
Précieux <precieuxjusticek@gmail.com>
2026-04-20 01:29:26 +0100

    modif

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const upload = multer({
 /* ===================================================== */
 /* ================= FIREBASE ADMIN ==================== */
 /* ===================================================== */
-const serviceAccount = require("./firebaseKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
